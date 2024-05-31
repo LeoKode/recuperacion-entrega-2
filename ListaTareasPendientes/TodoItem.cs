497f9ab540c0940881fb20dca785e0a8e6b1d834
LeoKode <fabbianromero@gmail.com>
2024-05-31 15:52:29 +0200

    Creación de una lista de tareas pendientes con Blazor

---
+++ ListaTareasPendientes/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ListaTareasPendientes
+{
+    // Define una clase que representa un elemento de una lista de tareas pendientes
+    public class TodoItem
+    {
+        // Título de la tarea pendiente
+        public string? Title { get; set; }
+        // Indica si la tarea está completada o no
+        public bool IsDone { get; set; } = false;
+    }
+}
